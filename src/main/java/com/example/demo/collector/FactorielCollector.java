039f1964842f79190906083386fd0c1a0282cfe4
wi22b050 <wi22b050@technikum-wien.at>
2023-06-28 18:49:37 +0200

    Added the Factoriel Calculator and the tests needed to ensure the proper function of the REST Api.

--- src/main/java/com/example/demo/collector/FactorielCollector.java
+++ src/main/java/com/example/demo/collector/FactorielCollector.java
@@ -1,6 +1,7 @@
 package com.example.demo.collector;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,18 @@ public class FactorielCollector {
     @GetMapping("/api/factorial/total")
     public String getTotalFactorials() {
         return String.valueOf(total);
+    }
+
+    @GetMapping("/api/factorial")
+    public int calcFactorial(@RequestParam(required = true) int number) {
+        int sum = 1;
+
+        for(int i = 1; i <= number; i++) {
+            sum *= i;
+        }
+
+        total += sum;
+        return sum;
     }
 
 }
--- src/test/java/com/example/demo/FactorielApplicationTests.java
+++ src/test/java/com/example/demo/FactorielApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import com.example.demo.collector.FactorielCollector;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class FactorielApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testFactorialOfNumber() {
+		FactorielCollector controller = new FactorielCollector();
+
+		int result = controller.calcFactorial(5);
+		assertEquals(120, result);
 	}
 
 }
